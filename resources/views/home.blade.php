6d44895012199ad9e26a1d82a79c01ea66d84edf
Imran <polboy777@gmail.com>
2020-03-16 18:36:49 +0600

    auth files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <ul>
+                            @foreach($notes as $note)
+                                <li>{{ $note->url }}</li>
+                            @endforeach
+                        </ul>
+
+                        You are logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
