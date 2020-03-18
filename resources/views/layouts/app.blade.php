e985a564476433355260c99f8db7ba5196a3cbc3
Imran <polboy777@gmail.com>
2020-03-18 11:52:53 +0600

    nav padding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body>
 <div id="app">
     <nav class="navbar navbar-expand navbar-light bg-white shadow-sm">
-        <div class="container">
+        <div class="container py-4">
             <a class="navbar-brand" href="{{ url('/') }}">
                 <button class="btn btn-outline-primary">New Note</button>
             </a>
