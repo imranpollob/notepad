bbe628361291bcea2218192e4286fb16420e0008
Imran <polboy777@gmail.com>
2020-03-18 11:14:02 +0600

    update script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
-    <title>Notepad Online - free store and share</title>
+    <title>Notepad Online - Free store and share</title>
     <meta name="description"
           content="Notepad online is a free tool for storing and sharing you notes. No registration required."/>
     <meta name="robots" content="”index," follow”/>
@@ -81,12 +81,12 @@
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
-{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"--}}
-{{--        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"--}}
-{{--        crossorigin="anonymous"></script>--}}
-{{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"--}}
-{{--        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"--}}
-{{--        crossorigin="anonymous"></script>--}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 <script src="{{ asset('js/script.js') }}"></script>
 
 @yield('javascript')
--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -17,3 +17,44 @@
 
     <div id="save-status" class="badge badge-success"></div>
 @endsection
+
+@section('javascript')
+    <script>
+        $(document).ready(function () {
+
+            //setup before functions
+            let typingTimer;                //timer identifier
+            let doneTypingInterval = 2000;  //time in ms (5 seconds)
+
+            //on keyup, start the countdown
+            $('#data, #title').keyup(function () {
+                $('#save-status').text('Saving ...');
+                clearTimeout(typingTimer);
+
+                typingTimer = setTimeout(doneTyping, doneTypingInterval);
+            });
+
+            //user is "finished typing," do something
+            function doneTyping() {
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                    url: '/{{ Request::path() }}',
+                    type: "POST",
+                    data: $('#note-form').serialize(),
+                    success: function (response) {
+                        console.log('saved');
+                        $('#save-status').text('Saved');
+                    }
+                });
+            }
+
+        });
+
+    </script>
+@endsection
+
