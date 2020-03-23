5e406955a78832cb7eac7c9964a4990248c1207c
Imran <polboy777@gmail.com>
2020-03-23 12:11:17 +0600

    remove unnecessary script fro note

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -70,37 +70,6 @@
     <script>
         $(document).ready(function () {
 
-            //setup before functions
-            let typingTimer;                //timer identifier
-            let doneTypingInterval = 2000;  //time in ms (5 seconds)
-
-            //on keyup, start the countdown
-            $('#data, #title').keyup(function () {
-                $('#save-status').text('Saving ...');
-                clearTimeout(typingTimer);
-
-                typingTimer = setTimeout(doneTyping, doneTypingInterval);
-            });
-
-            //user is "finished typing," do something
-            function doneTyping() {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-
-                $.ajax({
-                    url: '/{{ Request::path() }}',
-                    type: "PUT",
-                    data: $('#note-form').serialize(),
-                    success: function () {
-                        $('#save-status').text('Saved');
-                    }
-                });
-            }
-
-
             $('.copyToClipboard').click(function () {
                 let text = window.location.href;
 
